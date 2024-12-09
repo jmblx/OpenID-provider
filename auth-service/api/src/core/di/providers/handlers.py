0e70186986b3f600c719a7518fdd20e47a5c770d
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2024-12-10 00:14:15 +0300

    Feat: redirect ref

--- auth-service/api/src/core/di/providers/handlers.py
+++ auth-service/api/src/core/di/providers/handlers.py
@@ -4,6 +4,7 @@ from application.auth.handlers.auth_user_handler import AuthenticateUserHandler
 from application.auth.handlers.code_to_token_handler import CodeToTokenHandler
 from application.auth.handlers.refresh_tokens_handler import RefreshTokensHandler
 from application.auth.handlers.revoke_tokens_handler import RevokeTokensHandler
+from application.client.handlers.add_allowed_url import AddAllowedRedirectUrlCommandHandler
 from application.client.handlers.register_client_hadler import (
     RegisterClientHandler,
 )
@@ -29,3 +30,5 @@ class HandlerProvider(Provider):
     revoke_tokens_handler = provide(RevokeTokensHandler, scope=Scope.REQUEST)
     refresh_tokens_handler = provide(RefreshTokensHandler, scope=Scope.REQUEST)
     client_auth_validation_query_handler = provide(ClientAuthValidationQueryHandler, scope=Scope.REQUEST)
+    rename_client_command_handler = provide(ClientAuthValidationQueryHandler, scope=Scope.REQUEST)
+    add_allowed_redirect_url_command_handler = provide(AddAllowedRedirectUrlCommandHandler, scope=Scope.REQUEST)
