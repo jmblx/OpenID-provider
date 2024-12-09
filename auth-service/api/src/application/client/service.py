963192abf141c973de8a6006c22f05c1bebf38e9
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2024-12-10 00:10:24 +0300

    Feat: redirect ref

--- auth-service/api/src/application/client/service.py
+++ auth-service/api/src/application/client/service.py
@@ -1,10 +1,19 @@
 from application.client.commands.validate_client_request import ValidateClientRequest
 from application.client.interfaces.repo import ClientRepository
-from application.common.uow import Uow
 from domain.entities.client.model import Client
 from domain.entities.client.value_objects import ClientRedirectUrl, ClientID
 from domain.exceptions.client import ClientNotFound
 
-#
-# class ClientService:
-#
+
+class ClientService:
+    def __init__(self, client_repo: ClientRepository):
+        self.client_repo = client_repo
+
+    async def get_validated_client(self, data: ValidateClientRequest) -> Client:
+        client = await self.client_repo.get_by_id(ClientID(data.client_id))
+        if not client:
+            raise ClientNotFound()
+        await Client.validate_redirect_url(
+            allowed_redirect_urls=client.allowed_redirect_urls, redirect_url=ClientRedirectUrl(data.redirect_url)
+        )
+        return client
