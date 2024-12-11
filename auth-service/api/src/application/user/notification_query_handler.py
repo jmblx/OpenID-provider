759694f86e6c7463fb23056adabff6a650771757
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2024-12-11 14:28:24 +0300

    jma

--- auth-service/api/src/application/user/notification_query_handler.py
+++ auth-service/api/src/application/user/notification_query_handler.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from application.common.id_provider import IdentityProvider
 from application.user.interfaces.reader import UserReader
 from infrastructure.external_services.investments.service import InvestmentsService
@@ -15,8 +17,47 @@ class NotificationQueryHandler:
 
         notifications = []
         data = await self.investments_service.get_investments()
-
+        c = 0
         for strategy in user_strategies.strategies:
-            print(strategy)
+            c += 1
+            portfolio = strategy.portfolio
+            for key, value in portfolio.items():
+                if key not in data:
+                    continue  # Пропускаем, если данных по ключу нет
+                c_d = data[key]
+                today = datetime.now()
 
+                # Целевая дата
+                target_date = datetime(2024, 12, 10)
+
+                # Разница в днях
+                difference = (target_date - today).days
+
+                if today.strftime("%d.%m.%Y") in c_d:
+                    c_d = c_d[today.strftime("%d.%m.%Y")]
+                else:
+                    continue  # Пропускаем, если нет данных на сегодняшнюю дату
+
+                if difference >= 3:
+                    if target_date.strftime("%d.%m.%Y") in c_d:
+                        c_d = c_d.get(target_date.strftime("%d.%m.%Y"))
+                    else:
+                        continue  # Пропускаем, если нет данных на целевую дату
+
+                    for el in value:
+                        investment = c_d.get(el.get("name"))
+                        if investment:
+                            percent = investment.get("last_7_day_diff_in_%")
+                            if percent:
+                                percent = int(percent.replace("%", ""))
+                                if percent > 3:
+                                    notifications.append(
+                                        f"У вас есть инвестиции в {el.get('name')} с большой положительной разницей в процентном ростом {percent}%")
+                                elif percent < 3:
+                                    notifications.append(
+                                        f"У вас есть инвестиции в {el.get('name')} с большой отрицательной разницей в цене за последнюю неделю: {percent}%")
+                                else:
+                                    notifications.append(
+                                        f"У вас есть инвестиции в {el.get('name')} с маленькой разницей в процентным изменением {percent}%")
         return notifications
+
--- auth-service/api/src/infrastructure/external_services/investments/service.py
+++ auth-service/api/src/infrastructure/external_services/investments/service.py
@@ -19,3 +19,28 @@ class InvestmentsService:
         investments = await self.redis.get("data")
         investments_dict = json.loads(investments)
         return investments_dict
+
+    async def get_price_by_date_and_name(self, asset_type: str, asset_name: str) -> float:
+        investments = await self.get_investments()
+        today = date.today().strftime("%d.%m.%Y")
+
+        print(f"Получение цены для актива {asset_name} типа {asset_type} на дату {today}")
+
+        if asset_type not in investments:
+            print(f"Неизвестный тип актива: {asset_type}")
+            raise ValueError(f"Неизвестный тип актива: {asset_type}")
+
+        asset_data = investments[asset_type]
+        if today not in asset_data:
+            print(f"Данные по дате {today} не найдены для актива {asset_name}")
+            raise ValueError(f"Данные по дате {today} не найдены для актива {asset_name}")
+
+        asset_price_data = asset_data[today]
+        if asset_name not in asset_price_data:
+            print(f"Цена для актива {asset_name} не найдена на дату {today}")
+            raise ValueError(f"Цена для актива {asset_name} не найдена на дату {today}")
+
+        price = asset_price_data[asset_name]["price"]
+        price = float(price.replace("₽", "").replace(",", "."))
+        print(f"Цена для {asset_name} на {today}: {price} ₽")
+        return price
