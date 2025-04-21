aaa9b7ea6a626dc5d7c6f0437c3fc1d71d1d8238
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2025-04-22 00:40:00 +0300

    cd and pwd reset ui

--- auth-service/auth_service/src/presentation/web_api/middlewares/__init__.py
+++ auth-service/auth_service/src/presentation/web_api/middlewares/__init__.py
@@ -9,13 +9,13 @@ from ..config import TRACING
 def setup_middlewares(app: FastAPI):
     if TRACING:
         add_metrics_middleware(app)
-    app.add_middleware(
-        CORSMiddleware,
-        # allow_origins=["https://menoitami.ru"],  # или "*" для разработки
-        allow_origins=["*"],
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     # allow_origins=["https://menoitami.ru"],  # или "*" для разработки
+    #     allow_origins=["*"],
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
     # app.add_middleware(
     #     TrustedHostMiddleware,
     #     allowed_hosts=["menoitami.ru"]
