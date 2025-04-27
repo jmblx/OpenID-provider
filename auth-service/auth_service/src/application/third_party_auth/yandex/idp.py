29de25f9f217bacd9766b97d3d756558db1533d6
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2025-04-27 21:04:22 +0300

    yandex auth

--- auth-service/auth_service/src/application/third_party_auth/yandex/idp.py
+++ auth-service/auth_service/src/application/third_party_auth/yandex/idp.py
@@ -34,7 +34,7 @@ class YandexIdentityProvider(OauthIdentityProvider):
             raise ValueError(f"Ошибка обработки ответа: {str(e)}")
 
     async def get_current_user(self, oauth_token: OAuth2Token) -> User:
-        email = self.get_yandex_user_email(oauth_token)
+        email = await self.get_yandex_user_email(oauth_token)
         query = select(User).where(User.email == email)
         user = (await self.session.execute(query)).scalar()
         return user
