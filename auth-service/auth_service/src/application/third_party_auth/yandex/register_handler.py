fbad0cceb24fecc58c45b3bedd10168249eef080
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2025-04-28 16:19:04 +0300

    register with yandex

---
+++ auth-service/auth_service/src/application/third_party_auth/yandex/register_handler.py
@@ -0,0 +1,73 @@
+import secrets
+from dataclasses import dataclass
+from typing import cast
+
+from application.auth_as.register_user_auth_server_hander import RegisterUserResult
+from application.common.interfaces.email_confirmation_service import EmailConfirmationServiceI, UserRegisterNotifyData
+from application.common.interfaces.http_auth import HttpAuthServerService
+from application.common.interfaces.user_repo import UserRepository
+from application.common.uow import Uow
+from application.third_party_auth.common.idp import OAuth2Token
+from application.third_party_auth.common.password_generation import generate_random_password
+from application.third_party_auth.yandex.idp import YandexIdentityProvider
+from domain.common.services.pwd_service import PasswordHasher
+from domain.entities.user.model import User
+from domain.exceptions.user import UserAlreadyExistsError
+
+
+@dataclass
+class YandexRegisterCommand:
+    yandex_token: OAuth2Token
+
+
+class YandexRegisterHandler:
+    def __init__(
+        self,
+        user_repo: UserRepository,
+        hash_service: PasswordHasher,
+        uow: Uow,
+        email_confirmation_service: EmailConfirmationServiceI,
+        auth_server_service: HttpAuthServerService,
+        yandex_idp: YandexIdentityProvider
+    ):
+        self.user_repository = user_repo
+        self.hash_service = hash_service
+        self.uow = uow
+        self.email_confirmation_service = email_confirmation_service
+        self.auth_server_service = auth_server_service
+        self.yandex_idp = yandex_idp
+
+    async def handle(self, command: YandexRegisterCommand) -> RegisterUserResult:
+        email = await self.yandex_idp.get_yandex_user_email(command.yandex_token)
+        existing_user = await self.user_repository.get_by_email(
+            email=email,
+        )
+        if existing_user:
+            raise UserAlreadyExistsError(existing_user.email.value)
+        generated_password = generate_random_password()
+        user_id = User.generate_id()
+        user = User.create(
+            user_id=user_id,
+            email=email,
+            raw_password=generated_password,
+            password_hasher=self.hash_service,
+        )
+        await self.user_repository.save(user)
+        auth_tokens = await self.auth_server_service.create_and_save_tokens(
+            user
+        )
+        email_confirmation_token = secrets.token_urlsafe(32)
+        await self.email_confirmation_service.save_confirmation_token(
+            email_confirmation_token, user_id
+        )
+        notify_data: UserRegisterNotifyData = {
+            "email_confirmation_token": email_confirmation_token,
+            "email": command.email,
+        }
+        await self.email_confirmation_service.email_register_notify(
+            notify_data
+        )
+
+        await self.uow.commit()
+        result = {**auth_tokens, "user_id": user_id}
+        return cast(RegisterUserResult, result)
