6fff047e3253375ea6991f9743e9d3853dcb03e8
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2025-06-06 19:09:07 +0300

    aiobotocore inst of minio lib

--- auth-service/auth_service/src/infrastructure/external_services/storage/config.py
+++ auth-service/auth_service/src/infrastructure/external_services/storage/config.py
@@ -6,8 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+endpoint_url = os.getenv("MINIO_ENDPOINT_URL")
+
+
 class MinIOConfig(BaseModel):
-    endpoint_url: str = os.getenv("MINIO_ENDPOINT_URL")
+    endpoint_url = (
+        endpoint_url
+        if endpoint_url.startswith(("http://", "https://"))
+        else f"http://{endpoint_url}"
+    )
     access_key: str = os.getenv("MINIO_ACCESS_KEY")
     secret_key: str = os.getenv("MINIO_SECRET_KEY")
     user_avatar_bucket_name: str = os.getenv("MINIO_USER_AVATAR_BUCKET_NAME")
