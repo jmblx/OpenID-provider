816dbe5f7585b46577f03178b691b42ce6854494
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2025-06-07 04:11:35 +0300

    aiobotocore inst of minio lib

--- auth-service/auth_service/src/core/di/providers/settings.py
+++ auth-service/auth_service/src/core/di/providers/settings.py
@@ -8,16 +8,18 @@ from infrastructure.services.auth.config import JWTSettings
 
 
 class SettingsProvider(Provider):
-    storage_settings = provide(
-        MinIOConfig, scope=Scope.APP
-    )
+    @staticmethod
+    @provide(scope=Scope.APP)
+    def provide_storage_settings() -> MinIOConfig:
+        return MinIOConfig()
 
     @staticmethod
     @provide(scope=Scope.APP, provides=JWTSettings)
     def provide_jwt_settings() -> JWTSettings:
         return JWTSettings()
 
-    admin_settings = provide(
-        AdminSettings, scope=Scope.APP
-    )
+    @staticmethod
+    @provide(scope=Scope.APP)
+    def provide_admin_settings() -> AdminSettings:
+        return AdminSettings()
     # firebase_config = provide(FirebaseConfig().from_env, scope=Scope.APP, provides=FirebaseConfig
