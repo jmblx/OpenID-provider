fa1468cad61555721391fe659af77657a9e35ef8
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2025-06-09 22:19:51 +0300

    client load avatar

--- auth-service/auth_service/src/application/client/read_client_view_handler.py
+++ auth-service/auth_service/src/application/client/read_client_view_handler.py
@@ -1,9 +1,13 @@
+import logging
 from dataclasses import dataclass
 
 from application.client.common.client_reader import ClientReader
 from application.common.interfaces.imedia_storage import ClientS3StorageService
 from application.common.views.client_view import ClientView, ClientView
 from domain.entities.client.value_objects import ClientID
+
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -22,7 +26,8 @@ class ReadClientPageViewQueryHandler:
             ClientID(query.client_id)
         )
         if query.load_avatar:
-            presigned_url = self.client_storage.get_presigned_avatar_url(str(client_data))
+            presigned_url = self.client_storage.get_presigned_avatar_url(str(query.client_id))
+            # logger.info(f"client presigned_url={presigned_url}")
             client_data = dict(**client_data, avatar_url=presigned_url)
 
         return client_data
