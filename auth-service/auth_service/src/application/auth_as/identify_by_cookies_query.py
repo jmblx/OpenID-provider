92704927583a82b414a3f537e62941ec74d8a508
CoSm1C AvEnGeR <zhora.zhilin.06@mail.ru>
2025-06-16 13:20:31 +0300

    avatar cache fix

--- auth-service/auth_service/src/application/auth_as/identify_by_cookies_query.py
+++ auth-service/auth_service/src/application/auth_as/identify_by_cookies_query.py
@@ -19,11 +19,11 @@ class IdentifyByCookiesQueryHandler:
 
     async def handle(self) -> UserData:
         user = await self.idp.get_current_user()
-
+        avatar_update_timestamp = await self.s3_storage.get_user_avatar_update_timestamp(str(user.id.value))
         return {
             "email": user.email.value,
             "id": user.id.value,
             # "avatar_path": self.s3_storage.get_presigned_avatar_url(str(user.id.value)),
-            "avatar_update_timestamp": await self.s3_storage.get_user_avatar_update_timestamp(str(user.id.value)),
+            "avatar_update_timestamp": avatar_update_timestamp,
             "is_admin": user.is_admin,
         }
